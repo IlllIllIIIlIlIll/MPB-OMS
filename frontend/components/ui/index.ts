cdf51decc02d3aaacb8c1c59fe5f64b0b3d2f155
abelaprl <123937217+abelaprl@users.noreply.github.com>
2025-08-03 00:47:04 +0700

    feat: crowd detection (up & down treshold)

--- frontend/components/ui/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Badge } from './Badge';
-export { default as Input } from './Input';
-export { default as LoadingSpinner } from './LoadingSpinner';
-export { default as OccupancyIndicator } from './OccupancyIndicator';
-export { default as AlertCard } from './AlertCard'; 
